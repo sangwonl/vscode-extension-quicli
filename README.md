7e581b263ed9d3b8fa9609551bbe2e6d5df20f5a
gamz <gamzabaw@gmail.com>
2023-08-12 16:51:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# QuiCLI
+
+This is a simple extension that executes CLI commands written in the editor directly.
+
+## Features
+
+- [x] Run a command line in terminal
+- [ ] Extract a command line
+
+## Requirements
+
+No required dependencies
+
+## Extension Settings
+
+N/A
+
+## Known Issues
+
+N/A
+
+## Release Notes
+
+### 0.0.1
+
+Very initial version that runs a text on the cursor line through terminal
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+
+const TERMINAL_NAME = 'QuiCLI';
+
+const COMMAND_HANDLERS: {[command: string]: (args: any[]) => void} = {
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	'quicli.runCommandInTerminal': onRunCommandInTerminal,
+};
+
+function onRunCommandInTerminal(_args: any[]) {
+	const command = extractExecutableCommand(getTextAtCursorLine());
+
+	const term = getOrCreateTerminal(TERMINAL_NAME);
+	term.sendText(command, true);
+	term.show(true);
+}
+
+function getTextAtCursorLine(): string {
+	const activeEditor = vscode.window.activeTextEditor;
+	if (!activeEditor) {
+		return '';
+	}
+
+	const { text } = activeEditor.document.lineAt(activeEditor.selection.active.line);
+	return text;
+}
+
+function extractExecutableCommand(text: string): string {
+	return text;
+}
+
+function getOrCreateTerminal(name: string): vscode.Terminal {
+	let term = vscode.window.terminals.find(t => t.name === name);
+	if (!term) {
+		term = vscode.window.createTerminal(name);
+	}
+	return term;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	Object.entries(COMMAND_HANDLERS).forEach(([command, handler]) => {
+		const disposable = vscode.commands.registerCommand(command, handler);
+		context.subscriptions.push(disposable);
+	});
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
