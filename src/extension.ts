b7a4237564d048a4713576eb2dde68b9ac1c92fe
gamz <gamzabaw@gmail.com>
2023-08-12 19:33:19 +0900

    Setup prettier

--- src/extension.ts
+++ src/extension.ts
@@ -2,46 +2,48 @@ import * as vscode from 'vscode';
 
 const TERMINAL_NAME = 'QuiCLI';
 
-const COMMAND_HANDLERS: {[command: string]: (args: any[]) => void} = {
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	'quicli.runCommandInTerminal': onRunCommandInTerminal,
+const COMMAND_HANDLERS: { [command: string]: (args: any[]) => void } = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'quicli.runCommandInTerminal': onRunCommandInTerminal,
 };
 
 function onRunCommandInTerminal(_args: any[]) {
-	const command = extractExecutableCommand(getTextAtCursorLine());
+  const command = extractExecutableCommand(getTextAtCursorLine());
 
-	const term = getOrCreateTerminal(TERMINAL_NAME);
-	term.sendText(command, true);
-	term.show(true);
+  const term = getOrCreateTerminal(TERMINAL_NAME);
+  term.sendText(command, true);
+  term.show(true);
 }
 
 function getTextAtCursorLine(): string {
-	const activeEditor = vscode.window.activeTextEditor;
-	if (!activeEditor) {
-		return '';
-	}
+  const activeEditor = vscode.window.activeTextEditor;
+  if (!activeEditor) {
+    return '';
+  }
 
-	const { text } = activeEditor.document.lineAt(activeEditor.selection.active.line);
-	return text;
+  const { text } = activeEditor.document.lineAt(
+    activeEditor.selection.active.line,
+  );
+  return text;
 }
 
 function extractExecutableCommand(text: string): string {
-	return text;
+  return text;
 }
 
 function getOrCreateTerminal(name: string): vscode.Terminal {
-	let term = vscode.window.terminals.find(t => t.name === name);
-	if (!term) {
-		term = vscode.window.createTerminal(name);
-	}
-	return term;
+  let term = vscode.window.terminals.find(t => t.name === name);
+  if (!term) {
+    term = vscode.window.createTerminal(name);
+  }
+  return term;
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	Object.entries(COMMAND_HANDLERS).forEach(([command, handler]) => {
-		const disposable = vscode.commands.registerCommand(command, handler);
-		context.subscriptions.push(disposable);
-	});
+  Object.entries(COMMAND_HANDLERS).forEach(([command, handler]) => {
+    const disposable = vscode.commands.registerCommand(command, handler);
+    context.subscriptions.push(disposable);
+  });
 }
 
 // This method is called when your extension is deactivated
